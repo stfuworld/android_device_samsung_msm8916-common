e01602f1ec73793abc266435fde138b1676860e5
Jens Lody <jens@jenslody.de>
2021-02-12 20:26:58 +0100

    msm8916-common: manifest: add android.hardware.bluetooth{,.a2dp}@1.0
    Switch to android.hardware.bluetooth.a2dp@1.0-impl.mock

--- msm8916.mk
+++ msm8916.mk
@@ -83,7 +83,7 @@ endif
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.bluetooth.a2dp@1.0-impl
+    android.hardware.bluetooth.a2dp@1.0-impl.mock
 
 PRODUCT_PACKAGES += \
     libbase_shim \
